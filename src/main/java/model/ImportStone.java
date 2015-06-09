d1055d2c5a15ed6e45b76020d99348bc1fc78842
junkoliver <junk.oliver@gmx.de>
2015-06-09 23:00:51 +0200

    changes.

--- src/main/java/model/ImportStone.java
+++ src/main/java/model/ImportStone.java
@@ -12,10 +12,16 @@ import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.SerializerProvider;
 import org.codehaus.jackson.map.TypeSerializer;
+import org.jsefa.csv.annotation.CsvDataType;
+import org.jsefa.csv.annotation.CsvField;
+import org.jsefa.xml.annotation.XmlDataType;
+import org.jsefa.xml.annotation.XmlElement;
 
 
 @Entity
 @Table(name = "ImportStones")
+@XmlDataType(defaultElementName = "stone")
+@CsvDataType()
 public class ImportStone implements org.codehaus.jackson.map.JsonSerializableWithType {
 
 	@Id
@@ -23,78 +29,128 @@ public class ImportStone implements org.codehaus.jackson.map.JsonSerializableWit
 	@Column(name = "id")
 	private Long id;
 	
+    @CsvField(pos = 1)
+    @XmlElement(name = "InvNr1", pos = 1)
 	@Column(name = "InvNr")
 	private String invNr;
-	
+
+    @CsvField(pos = 2)
+    @XmlElement(name = "MusId", pos = 2)
 	@Column(name = "MusId")
 	private Long musId;
 
+    @CsvField(pos = 3)
+    @XmlElement(name = "Link", pos = 3)
 	@Column(name = "Link")
 	private String link;
 
+    @CsvField(pos = 4)
+    @XmlElement(name = "InvNr2", pos = 4)
     @Column(name = "InvNr2")
     private String InvNr2;
 
+    @CsvField(pos = 5)
+    @XmlElement(name = "Bild", pos = 5)
     @Column(name = "Bild")
     private String Bild;
 
+    @CsvField(pos = 6)
+    @XmlElement(name = "Titel", pos = 6)
     @Column(name = "Titel")
     private String Titel;
 
+    @CsvField(pos = 7)
+    @XmlElement(name = "Masze", pos = 7)
     @Column(name = "Masze")
     private String Maﬂe;
 
+    @CsvField(pos = 8)
+    @XmlElement(name = "Hoehe", pos = 8)
     @Column(name = "Hoehe")
     private String Hoehe;
 
+    @CsvField(pos = 9)
+    @XmlElement(name = "Breite", pos = 9)
     @Column(name = "Breite")
     private String Breite;
 
+    @CsvField(pos = 10)
+    @XmlElement(name = "Tiefe", pos = 10)
     @Column(name = "Tiefe")
     private String Tiefe;
 
+    @CsvField(pos = 11)
+    @XmlElement(name = "Unit", pos = 11)
     @Column(name = "Unit")
     private String Unit;
 
+    @CsvField(pos = 12)
+    @XmlElement(name = "Material", pos = 12)
     @Column(name = "Material")
     private String Material;
 
+    @CsvField(pos = 13)
+    @XmlElement(name = "MaterialId", pos = 13)
     @Column(name = "MaterialId")
     private String MaterialId;
 
+    @CsvField(pos = 14)
+    @XmlElement(name = "Datierung", pos = 14)
     @Column(name = "Datierung")
     private String Datierung;
 
+    @CsvField(pos = 15)
+    @XmlElement(name = "Erdzeitalter", pos = 15)
     @Column(name = "Erdzeitalter")
     private String Erdzeitalter;
 
+    @CsvField(pos = 16)
+    @XmlElement(name = "ErdzeitalterId", pos = 16)
     @Column(name = "ErdzeitalterId")
     private String ErdzeitalterId;
 
+    @CsvField(pos = 17)
+    @XmlElement(name = "Herkunft", pos = 17)
     @Column(name = "Herkunft")
     private String Herkunft;
 
+    @CsvField(pos = 18)
+    @XmlElement(name = "HerkunftCoord", pos = 18)
     @Column(name = "HerkunftCoord")
     private String HerkunftCoord;
 
+    @CsvField(pos = 19)
+    @XmlElement(name = "HerkunftId", pos = 19)
     @Column(name = "HerkunftId")
     private String HerkunftId;
 
+    @CsvField(pos = 20)
+    @XmlElement(name = "HerkunftGeoId", pos = 20)
     @Column(name = "HerkunftGeoId")
     private String HerkunftGeoId;
 
+    @CsvField(pos = 21)
+    @XmlElement(name = "Fundort", pos = 21)
     @Column(name = "Fundort")
     private String Fundort;
 
+    @CsvField(pos = 22)
+    @XmlElement(name = "FundortCoord", pos = 22)
     @Column(name = "FundortCoord")
     private String FundortCoord;
 
+    @CsvField(pos = 23)
+    @XmlElement(name = "FundortId", pos = 23)
     @Column(name = "FundortId")
     private String FundortId;
 
+    @CsvField(pos = 24)
+    @XmlElement(name = "FundortGeoId", pos = 24)
     @Column(name = "FundortGeoId")
     private String FundortGeoId;
 
+    @CsvField(pos = 25)
+    @XmlElement(name = "Kommentar", pos = 25)
     @Column(name = "Kommentar")
     private String Kommentar;
 
---
+++ src/main/java/service/ImportService.java
@@ -0,0 +1,80 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+
+import javax.servlet.ServletContext;
+
+import model.ImportStone;
+
+import org.jsefa.xml.XmlDeserializer;
+import org.jsefa.xml.XmlIOFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.support.ServletContextResource;
+
+import dao.ImportStoneDao;
+
+
+@Service
+public class ImportService {
+	
+	
+	@Autowired
+	private ImportStoneDao importStoneDao;
+	@Autowired 
+	private ServletContext servletContext;
+	@Transactional
+	public void importtoDB() throws MalformedURLException, IOException{
+		
+		/*FileSystemResource resource = new FileSystemResource("/WEB-INF/resource/hackathon2015-daten-stadtmuseumberlin.csv");
+		File file = resource.getFile();
+
+		byte[] bytes;
+		
+		*/
+		
+		 InputStream inputStream = null;
+		try {
+			ServletContextResource resource = new ServletContextResource(servletContext, 
+				    "/WEB-INF/resource/hackathon2015-daten-stadtmuseumberlin.csv");
+			
+            //inputStream = servletContext.getResourceAsStream("/WEB-INF/resource/hackathon2015-daten-stadtmuseumberlin.csv");
+            inputStream=resource.getInputStream();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+			StringWriter writer = new StringWriter();
+            
+            
+            /*
+			bytes = Files.readAllBytes(file.toPath());
+			writer.write( new String(bytes,"UTF-8"));*/
+			writer.write(bufferedReader.readLine());
+			XmlDeserializer deserializer = XmlIOFactory.createFactory(ImportStone.class).createDeserializer();
+			StringReader reader = new StringReader(writer.toString());
+			deserializer.open(reader);
+			while (deserializer.hasNext()) {
+			    ImportStone p = deserializer.next();
+			    importStoneDao.persist(p);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
