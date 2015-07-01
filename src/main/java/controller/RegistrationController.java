70a2c40fc2d13ca131333f698eb8f216e973c949
bennySwiers <swiers.benjamin@googlemail.com>
2015-07-01 17:33:02 +0200

    * Weiterletung zum HotOrNotController so verändert, dass aktueller User übergeben wird

--- src/main/java/controller/RegistrationController.java
+++ src/main/java/controller/RegistrationController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import service.DatabaseService;
+
 /**
  * Created by voskart on 11.06.15.
  */
@@ -25,44 +26,42 @@ import service.DatabaseService;
 public class RegistrationController {
 
 	@Autowired
-    private DatabaseService dbservice;
+	private DatabaseService dbservice;
 
-    @Autowired
-    private ServletContext servletContext;
-    private static final Logger logger = Logger.getLogger(String.valueOf(RegistrationController.class));
+	@Autowired
+	private ServletContext servletContext;
+	private static final Logger logger = Logger.getLogger(String
+			.valueOf(RegistrationController.class));
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String printWelcome(HttpServletRequest req, ModelMap model) throws XQException, IOException {
-        logger.info("Successfully submitted registration form");
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        String passwordHash = LoginController.encryptPassword(password);
-        // Create the user-object if needed
-        Integer newID = dbservice.getLastUserID();
+	@RequestMapping(method = RequestMethod.POST)
+	public String printWelcome(HttpServletRequest req, ModelMap model)
+			throws XQException, IOException {
+		logger.info("Successfully submitted registration form");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String passwordHash = LoginController.encryptPassword(password);
+		// Create the user-object if needed
+		Integer newID = dbservice.getLastUserID();
 
-        User user = new User(username, passwordHash, newID + 1);
-        // Save user to XML-file
-        saveUser(user);
-        // Redirect user to needed page, you can also pass the user-object once again
-        model.addAttribute(model.addAttribute("username", username));
-        for (User u: dbservice.getAllUsers()){
-        	logger.info("User: "+u+"\n");
-        }
-        
-        return "HotOrNot";
-    }
-    
-    private void saveUser(User user) throws XQException, IOException{
-    	try {
+		User user = new User(username, passwordHash, newID + 1);
+		// Save user to XML-file
+		saveUser(user);
+		req.getSession().setAttribute("user", user);
+
+		return "redirect:HotOrNot";
+	}
+
+	private void saveUser(User user) throws XQException, IOException {
+		try {
 			this.dbservice.insertNewUserData(user);
 		} catch (BaseXException e) {
 			RegistrationController.logger.info(e.getMessage());
 		}
-    }
+	}
 
-    // Method for get-requests, to display the actual form
-    @RequestMapping(method = RequestMethod.GET)
-    public String showRegistration(ModelMap model) {
-        return "registration";
-    }
+	// Method for get-requests, to display the actual form
+	@RequestMapping(method = RequestMethod.GET)
+	public String showRegistration(ModelMap model) {
+		return "registration";
+	}
 }
